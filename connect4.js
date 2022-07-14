10ed3cedb3cfc766656a6d3cb59b7bffab97b0cb
Brenda Liu <brenda.liu24@gmail.com>
2022-07-13 17:24:09 -0700

    create if win function

--- connect4.js
+++ connect4.js
@@ -94,6 +94,10 @@ function placeInTable(y, x) {
 
 function endGame(msg) {
   // TODO: pop up alert message
+  setTimeout(() => {
+    alert(msg)
+  }, 500);
+
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -145,8 +149,24 @@ function checkForWin() {
 
     // TODO: Check four cells to see if they're all legal & all color of current
     // player
+    for (let i = 0; i < cells.length; i++){
+      let cell = cells[i];
 
-  }
+      if (cell[0] < 0 || cell[0] >= HEIGHT || cell[1] < 0 || cell[1] >= WIDTH){
+        return false;
+      }
+      let firstCell = cells[0];
+      let firstVal = board[firstCell[0]][firstCell[1]];
+      let nextVal = board[cell[0]][cell[1]];
+      if(firstVal !== nextVal || firstVal === null || nextVal === null){
+        return false;
+      }
+
+
+    }
+    return true;
+
+   }
 
   // using HEIGHT and WIDTH, generate "check list" of coordinates
   // for 4 cells (starting here) for each of the different
@@ -159,9 +179,9 @@ function checkForWin() {
       // [ [y, x], [y, x], [y, x], [y, x] ]
 
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert;
-      let diagDL;
-      let diagDR;
+      let vert = [[y,x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDL = [[y,x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      let diagDR = [[y,x], [y + 1, x - 1], [y + 2,x - 2], [y + 3,x - 3]];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
